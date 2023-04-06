5b07dce4508dd2dce15200c4a8149de4149b1ef9
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-06 22:29:24 +0500

    Initial commit

---
+++ AlertSwiftUI/AlertSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSwiftUIApp.swift
+//  AlertSwiftUI
+//
+//  Created by sss on 06.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AlertSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
